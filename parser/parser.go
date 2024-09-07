5f3d5dcabca8406974a37de4b587c6a1cd5f8fcc
pro0o <pp48041721@student.ku.edu.np>
2024-09-08 00:57:06 +0545

    feat(parser): parse stmt and let stmt.

---
+++ parser/parser.go
@@ -0,0 +1,90 @@
+package parser
+
+import (
+	"main/ast"
+	"main/lexer"
+	"main/token"
+)
+
+type Parser struct {
+	l lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(lex *lexer.Lexer) *Parser {
+	p := &Parser{l: *lex}
+
+	// fill in cur and peek token
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	// advance the token positiion for ident verify
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+	// TODO: skipping the expressions [eval]
+	// until encountering a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		// set current token to peek and peek -> next
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
+}
